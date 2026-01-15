dc547ac7fec5108098952b7a5be1c55015cb455e
Swotboysandy <swotboysandy@gmail.com>
2026-01-15 20:10:34 +0530

    Revert .htaccess and standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'standalone',
   reactCompiler: true,
   images: {
     unoptimized: true, // Disable image optimization to bypass proxy issues
