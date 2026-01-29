020513870ea199b58f622c0245c4219973115f62
Swotboysandy <swotboysandy@gmail.com>
2026-01-29 19:45:39 +0530

    Add Microsoft Clarity and Google Analytics tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,31 @@ export default function RootLayout({
             };
           `
         }} />
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ui8hxy360h");
+          `
+        }} />
+
+        {/* Google Analytics (GA4) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-D85JG8ZSQ1"
+        />
+        <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-D85JG8ZSQ1');
+          `
+        }} />
       </body>
     </html>
   );
